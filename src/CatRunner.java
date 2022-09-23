b790c79381ef32e6e9df9dd7a426fbf16f647e55
masuduls <masuduls@nycstudents.net>
2022-09-23 10:50:02 -0400

    Initial commit

---
+++ src/CatRunner.java
@@ -0,0 +1,9 @@
+public class CatRunner {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Tom", 8, 12.2);
+        cat1.introduce();
+
+        Cat cat2 = new Cat("Anna", 4, 8.2);
+        cat2.introduce();
+    }
+}
